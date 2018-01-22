9fc375a60094092bc08eed972810b8d49c9e0988
Siena Aguayo <siena@indiegogo.com>
2018-01-21 19:04:04 -0800

    S02.01-Exercise-Networking

--- S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/utilities/NetworkUtils.java
+++ S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/utilities/NetworkUtils.java
@@ -15,9 +15,11 @@
  */
 package com.example.android.sunshine.utilities;
 
+import android.net.Uri;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
@@ -65,8 +67,15 @@ public final class NetworkUtils {
      * @return The URL to use to query the weather server.
      */
     public static URL buildUrl(String locationQuery) {
-        // TODO (1) Fix this method to return the URL used to query Open Weather Map's API
-        return null;
+        Uri uri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+            .appendQueryParameter(QUERY_PARAM, locationQuery).build();
+        URL url = null;
+        try {
+            url = new URL(uri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return url;
     }
 
     /**
