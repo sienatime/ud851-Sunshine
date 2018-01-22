0d112d1371173b67eacc5d0ddabc8950e632c3b6
Siena Aguayo <siena@indiegogo.com>
2018-01-21 19:15:26 -0800

    Kotlin Lesson 5.23 S04.01-Exercise-LaunchNewActivity complete todos

---
+++ S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/DetailActivity.kt
@@ -0,0 +1,17 @@
+package com.example.android.sunshine
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_detail.*
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+
+    }
+
+}
--- S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/MainActivity.kt
+++ S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/MainActivity.kt
@@ -16,6 +16,7 @@
 package com.example.android.sunshine
 
 import android.content.Context
+import android.content.Intent
 import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -114,11 +115,11 @@ class MainActivity : AppCompatActivity(), ForecastAdapterOnClickHandler {
      */
     override fun onClick(weatherForDay: String) {
         val context = this
-        // TODO (1) Create a new Activity called DetailActivity using Android Studio's wizard
-        // TODO (2) Change the root layout of activity_detail.xml to a FrameLayout and remove unnecessary xml attributes
-        // TODO (3) Remove the Toast and launch the DetailActivity using an explicit Intent
-        Toast.makeText(context, weatherForDay, Toast.LENGTH_SHORT)
-                .show()
+        // Completed TODO (1) Create a new Activity called DetailActivity using Android Studio's wizard
+        // Completed TODO (2) Change the root layout of activity_detail.xml to a FrameLayout and remove unnecessary xml attributes
+        // Completed TODO (3) Remove the Toast and launch the DetailActivity using an explicit Intent
+        val intent = Intent(context, DetailActivity::class.java)
+        startActivity(intent)
     }
 
     /**
