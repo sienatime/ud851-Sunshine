d79ea600befd3c0dde47c92909213110b2214b45
Siena Aguayo <siena@indiegogo.com>
2018-02-11 13:11:13 -0800

    Kotlin Lesson 7.1 completed todos

--- S07.01-Exercise-DatabaseCreation/app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.kt
+++ S07.01-Exercise-DatabaseCreation/app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.kt
@@ -15,12 +15,37 @@
  */
 package com.example.android.sunshine.data
 
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.provider.BaseColumns
+
 /**
  * Manages a local database for weather data.
  */
-// TODO (11) Extend SQLiteOpenHelper from WeatherDbHelper
-class WeatherDbHelper//  TODO (12) Create a public static final String called DATABASE_NAME with value "weather.db"
-//  TODO (13) Create a private static final int called DATABASE_VERSION and set it to 1
-//  TODO (14) Create a constructor that accepts a context and call through to the superclass constructor
-//  TODO (15) Override onCreate and create the weather table from within it
-//  TODO (16) Override onUpgrade, but don't do anything within it yet
+class WeatherDbHelper : SQLiteOpenHelper {
+    constructor(context: Context) : super(context, DATABASE_NAME, null, DATABASE_VERSION)
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL("""
+CREATE TABLE ${WeatherContract.WeatherEntry.TABLE_NAME} (
+${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT,
+${WeatherContract.WeatherEntry.COLUMN_DATE} INTEGER,
+${WeatherContract.WeatherEntry.COLUMN_WEATHER_ID} INTEGER,
+${WeatherContract.WeatherEntry.COLUMN_MIN_TEMP} REAL,
+${WeatherContract.WeatherEntry.COLUMN_MAX_TEMP} REAL,
+${WeatherContract.WeatherEntry.COLUMN_HUMIDITY} REAL,
+${WeatherContract.WeatherEntry.COLUMN_PRESSURE} REAL,
+${WeatherContract.WeatherEntry.COLUMN_WIND_SPEED} REAL,
+${WeatherContract.WeatherEntry.COLUMN_DEGREES} REAL)
+""")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+    }
+
+    companion object {
+        const val DATABASE_NAME = "weather.db"
+        const val DATABASE_VERSION = 1
+    }
+}
